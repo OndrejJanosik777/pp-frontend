dd761d864a7eb1fc9ef94857b29649f44d8e2b31
Ondrej Janosik <ondrej.janosik@gmail.com>
2023-10-17 07:42:51 +0200

    test center page created

--- src/pages/p05-test-center/index.js
+++ src/pages/p05-test-center/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+
+import home from './assets/home.png';
+import small_arrow_down from './assets/small_arrow_down.png';
+
 import C01_NAVBAR from '../../components/c01-nav-bar';
 import C02_SIDEBAR from '../../components/c02-side-bar';
 import './index.scss';
@@ -7,7 +11,16 @@ const P05_TEST_CENTER = () => {
     return ( <div className='p05-test-center'>
         <C01_NAVBAR />
         <C02_SIDEBAR />
-        hello world from page test center.
+        <div className='p05-center-section'>
+            <img className='p05-img-home' src={home} alt='' />
+            <button className='p05-button-container'>
+                <div className='p05-button-name'>Quick Links</div>
+                <img className='p05-img' src={small_arrow_down} alt='' />
+            </button>
+            <div className='p05-main-section' id='main-section' name='main-section'>
+                hello
+            </div>
+        </div>
     </div> );
 }
  
